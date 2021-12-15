9e6c1a409f4a884a3fe7a3e76ce22ef9b1118405
Patrick Leugers <patrick.leugers@gmail.com>
2021-12-15 15:20:06 +0100

    add startTime method to Stopwatch

--- timerx-example/src/main/java/com/arsvechkarev/timerxexample/StopwatchFragment.kt
+++ timerx-example/src/main/java/com/arsvechkarev/timerxexample/StopwatchFragment.kt
@@ -6,10 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_stopwatch.btn_reset
-import kotlinx.android.synthetic.main.fragment_stopwatch.btn_start
-import kotlinx.android.synthetic.main.fragment_stopwatch.btn_stop
-import kotlinx.android.synthetic.main.fragment_stopwatch.text_time
+import kotlinx.android.synthetic.main.fragment_stopwatch.*
 import timerx.Stopwatch
 import timerx.StopwatchBuilder
 import java.util.concurrent.TimeUnit
@@ -18,44 +15,47 @@ import java.util.concurrent.TimeUnit
  * Example of using a stopwatch
  */
 class StopwatchFragment : Fragment() {
-  
-  private lateinit var stopwatch: Stopwatch
-  
-  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                            savedInstanceState: Bundle?): View? {
-    return inflater.inflate(R.layout.fragment_stopwatch, container, false)
-  }
-  
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-    
-    stopwatch = StopwatchBuilder()
-        .startFormat("SS:LLL")
-        .actionWhen(5, TimeUnit.SECONDS) { showToast("5s passed") }
-        .actionWhen(10, TimeUnit.SECONDS) { showToast("10s passed") }
-        .actionWhen(20, TimeUnit.SECONDS) { showToast("20s passed") }
-        .onTick { time ->
-          text_time.text = time
+
+    private lateinit var stopwatch: Stopwatch
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_stopwatch, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        stopwatch = StopwatchBuilder()
+            .startFormat("SS:LLL")
+            .startTime(0L, TimeUnit.SECONDS)
+            .actionWhen(5, TimeUnit.SECONDS) { showToast("5s passed") }
+            .actionWhen(10, TimeUnit.SECONDS) { showToast("10s passed") }
+            .actionWhen(20, TimeUnit.SECONDS) { showToast("20s passed") }
+            .onTick { time ->
+                text_time.text = time
+            }
+            .build()
+
+        text_time.text = stopwatch.formattedStartTime
+
+        btn_start.setOnClickListener { stopwatch.start() }
+
+        btn_stop.setOnClickListener {
+            stopwatch.stop()
+            showToast("Current time in seconds = " + stopwatch.getTimeIn(TimeUnit.SECONDS))
         }
-        .build()
-    
-    text_time.text = stopwatch.formattedStartTime
-    
-    btn_start.setOnClickListener { stopwatch.start() }
-    
-    btn_stop.setOnClickListener {
-      stopwatch.stop()
-      showToast("Current time in seconds = " + stopwatch.getTimeIn(TimeUnit.SECONDS))
+
+        btn_reset.setOnClickListener {
+            text_time.text = stopwatch.formattedStartTime
+            stopwatch.reset()
+        }
     }
-    
-    btn_reset.setOnClickListener {
-      text_time.text = stopwatch.formattedStartTime
-      stopwatch.reset()
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        stopwatch.release()
     }
-  }
-  
-  override fun onDestroyView() {
-    super.onDestroyView()
-    stopwatch.release()
-  }
 }
